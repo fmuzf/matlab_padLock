a8c9dd926872b9e35f392bff5b743d8ba8ae77e7
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-08-07 11:39:18 -0800

    added beginning of test suite

---
+++ +padLock/+tests/testSuite1.m
@@ -0,0 +1,58 @@
+function test_suite = testSuite1()
+%TESTLOCKCREATION Summary of this function goes here
+%   Detailed explanation goes here
+initTestSuite;
+end
+
+
+function lockfilename = setup()
+goldpath = padLock.tests.getDataPath();
+lockfilename = [goldpath '/LockCreation.test'];
+end
+
+
+function teardown(lockfilename)
+delete(lockfilename);
+end
+
+
+function testLockCreation(lockfilename)
+% Test that Lockfile is Created
+stats = @(x)padLock.tests.StatusCodes(x);
+lf = padLock.LockFile(lockfilename, stats);
+lf.setStatus(padLock.tests.StatusCodes.LOCKED);
+assertTrue(logical(exist(lockfilename, 'file')));
+end
+
+
+function testLockNoCreation(lockfilename)
+% Test that No file is created on construction
+stats = @(x)padLock.tests.StatusCodes(x);
+lf = padLock.LockFile(lockfilename, stats);
+assertFalse(logical(exist(lockfilename, 'file')));
+end
+
+
+function testDelete(lockfilename)
+% Test that file is deleted by deleteLock
+stats = @(x)padLock.tests.StatusCodes(x);
+lf = padLock.LockFile(lockfilename, stats);
+lf.setStatus(0);
+lf.deleteLock();
+assertFalse(logical(exist(lockfilename, 'file')));
+end
+
+
+function testLocksToLocked(lockfilename)
+% check that correct file is created
+goldpath = padLock.tests.getDataPath();
+goldfile = [goldpath '/locked.gold'];
+
+stats = @(x)padLock.tests.StatusCodes(x);
+lf = padLock.LockFile(lockfilename, stats);
+lf.setStatus(padLock.tests.StatusCodes.LOCKED);
+
+gen = fileread(lockfilename);
+gold = fileread(goldfile);
+assertEqual(gen, gold);
+end
